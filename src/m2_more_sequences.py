90b9d183404218532983b6d5b51c3a1b5310a296
delemojw <delemojw@rose-hulman.edu>
2018-04-19 08:23:04 -0400

    Started CODE

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -10,8 +10,8 @@ for ITERATING through SEQUENCES, including selections from:
   -- Looking at two sequences in parallel
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jabari-Aman Delemore.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -102,6 +102,12 @@ def shortest_string(strings):
     Type hints:
       :type strings: list[str]   or tuple(str)
     """
+    # y = []
+    # for k in range(len(strings)):
+    #     x = strings[k]
+    #     y = y + [len(x)]
+    #     if y[k] == min(y[k]):
+    #         return y[k]
     # ------------------------------------------------------------------
     # TODO: 2. Implement and test this function.
     #     The testing code is already written for you (above).
